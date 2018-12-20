6a6d3a3f54bd054a98d94ae2ec1f07e2bdbac271
Martin Garton <garton@gmail.com>
2018-12-20 10:05:34 +0000

    Move tests around
    This moves tests into the most appropriate files.

--- freezer_source_test.go
+++ freezer_source_test.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/uw-labs/straw"
@@ -54,6 +55,92 @@ func TestConsumeMessagesCorruptDataTest(t *testing.T) {
 			assert.EqualError(source.ConsumeMessages(context.Background(), func([]byte) error { return nil }), test.errMsg)
 		})
 	}
+}
+
+func TestAwaitInputFile(t *testing.T) {
+	assert := assert.New(t)
+
+	ss := straw.NewMemStreamStore()
+
+	source := NewMessageSource(ss, MessageSourceConfig{Path: "/", PollPeriod: 20 * time.Millisecond})
+
+	messages := make(chan []byte, 1)
+	consumeErrors := make(chan error, 1)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+	defer cancel()
+	go func() {
+		consumeErrors <- source.ConsumeMessages(ctx, func(m []byte) error { messages <- m; return nil })
+	}()
+
+	time.Sleep(30 * time.Millisecond)
+	sink, err := NewMessageSink(ss, MessageSinkConfig{Path: "/", MaxUnflushedTime: 5 * time.Millisecond})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NoError(sink.PutMessage([]byte{1, 2, 3, 4, 5}))
+
+	select {
+	case <-ctx.Done():
+		t.Error("timeout before message")
+	case m := <-messages:
+		cancel()
+		assert.Equal([]byte{1, 2, 3, 4, 5}, m)
+		assert.NoError(sink.Close())
+	}
+
+	if err := <-consumeErrors; err != nil && err != ctx.Err() {
+		t.Errorf("error during consume %v", err)
+	}
+
+}
+
+func TestSourceContextCancelDuringRead(t *testing.T) {
+
+	assert := assert.New(t)
+
+	ss := straw.NewMemStreamStore()
+
+	sink, err := NewMessageSink(ss, MessageSinkConfig{Path: "/foo/bar/baz"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NoError(sink.PutMessage([]byte{1}))
+	assert.NoError(sink.PutMessage([]byte{2}))
+	assert.NoError(sink.Close())
+
+	source := NewMessageSource(ss, MessageSourceConfig{Path: "/foo/bar/baz"})
+
+	messages := make(chan []byte)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+	consumeErr := make(chan error, 1)
+	defer cancel()
+	go func() {
+		consumeErr <- source.ConsumeMessages(ctx, func(m []byte) error {
+			select {
+			case messages <- m:
+				return nil
+			case <-ctx.Done():
+				return nil
+			}
+		})
+	}()
+
+	select {
+	case <-ctx.Done():
+		t.Error("timeout before message")
+	case m := <-messages:
+		assert.Equal([]byte{1}, m)
+		cancel()
+	}
+
+	if err := <-consumeErr; err != nil {
+		t.Error(err)
+	}
+
 }
 
 func length(l int) []byte {
